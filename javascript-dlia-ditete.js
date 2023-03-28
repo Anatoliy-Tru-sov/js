496a056da2a2a5ec1c43bcd0d07bae5fe1b6ccc5
Anatoliy-Tru-sov <117832727+Anatoliy-Tru-sov@users.noreply.github.com>
2023-03-28 17:59:50 +0700

    Create javascript-dlia-ditete.js

---
+++ javascript-dlia-ditete.js
@@ -0,0 +1,42 @@
+var score = 10;
+score += 7;
+17
+score -= 3;
+14
+
+var balloons = 100;
+balloons *= 2;
+200
+
+var balloons = 100;
+balloons /= 4;
+25
+
+var codeWord1 = "обернись";
+var codeWord2 = "неужели";
+var codeWord3 = "огурцы";
+var codeWord4 = "липкие";
+var codeWord5 = "?!";
+codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1] + codeWord5[1];
+"беги!"
+
+var myTopThreeDinosaurs = ["Триннозавр", "Велоцираптор", "Стегозавр"];
+var dinosaurs = [
+    "Триннозавр",
+    "Велоцираптор",
+    "Стегозавр",
+    "Трицератопс",
+    "Брахиозавр",
+    "Птеранодон",
+    "Апатозавр",
+    "Диплодок",
+    "Комсогнат",
+    ];
+
+    var isWeekend = false;
+var hadShower = true;
+var hasApple = false;
+var hasOrange = true;
+var shouldGoToSchool = !isWeekend && hadShower && (hasApple || hasOrange);
+shouldGoToSchool;
+True
